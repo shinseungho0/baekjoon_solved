d19ea3c44d6b220051d76c0ecf7ee6a41121e176
shinseungho0 <sin339012@naver.com>
2023-09-11 13:24:36 +0900

    업로드했어용

---
+++ 11653. 소인수분해/11653.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int i = 2;
+
+	while (n != 1) {
+
+		if (n % i == 0) {
+			cout << i << endl;
+			n = n / i;
+		}
+		else {
+			i++;
+		}
+	}
+}
