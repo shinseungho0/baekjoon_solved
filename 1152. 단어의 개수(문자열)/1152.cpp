540a29f46026c70f4ee20c8daef682dd7e248275
shinseungho0 <sin339012@naver.com>
2023-09-13 12:55:03 +0900

    업로드 했당

---
+++ 1152. 단어의 개수(문자열)/1152.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin, str);
+	int count = 0;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ') count++;
+	}
+	if (str[0] == ' ') count--;
+	if (str[str.length() - 1] == ' ') count--;
+	cout << count + 1;
+}
