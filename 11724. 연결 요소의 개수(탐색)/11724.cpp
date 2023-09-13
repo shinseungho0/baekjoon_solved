7175d8ca17382963412dfa91e8e6e0226f1a79a3
shinseungho0 <sin339012@naver.com>
2023-09-13 16:40:15 +0900

    업로드!

---
+++ 11724. 연결 요소의 개수(탐색)/11724.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFS(int index);
+
+vector<vector<int>>v(1001, vector<int>(1001, 0));
+vector<bool>visit;
+
+int main() {
+	int n, m; // n은 노드의 개수, m은 간선의 개수
+	cin >> n >> m;
+	
+	visit = vector<bool>(n + 1, false);
+
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+
+	int count = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (visit[i] == false) {
+			DFS(i);
+			count++;
+		}
+	}
+	cout << count;
+}
+
+void DFS(int index) {
+	visit[index] = true;
+
+	for (int i = 0; i < v[index].size(); i++) {
+		int temp = v[index][i];
+		if (visit[temp] == false) {
+			DFS(temp);
+		}
+	}
+}
