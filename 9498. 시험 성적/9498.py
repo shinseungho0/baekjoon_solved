ae2be28e78b18c94d5028110322c901dc9063d64
shinseungho0 <sin339012@naver.com>
2023-10-30 16:06:12 +0900

    기여만 하자

---
+++ 9498. 시험 성적/9498.py
@@ -0,0 +1,11 @@
+a = int(input()) # int형 입력받기
+if a <= 100 and a >= 90:
+  print('A')
+elif 80 <= a <= 89:
+  print('B')   
+elif 70 <= a <= 79:
+  print('C')  
+elif 60 <= a <= 69:
+  print('D')
+else :
+  print('F')    
