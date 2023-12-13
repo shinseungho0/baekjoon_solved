10d75d26f4407f8e7a05eb139572f592eabe4359
shinseungho0 <sin339012@naver.com>
2023-12-13 19:59:28 +0900

    업로드

---
+++ 2744. 대소문자 바꾸기/2744.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 97) str[i] = str[i] - 32;
+		else str[i] = str[i] + 32;
+	}
+	cout << str;
+}
